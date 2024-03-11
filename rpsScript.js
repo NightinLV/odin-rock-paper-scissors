1e30b23ecb331a25c368efeb71100a761ed2ebaa
NightinLV <laurvrincianu@gmail.com>
2024-03-12 00:55:20 +0100

    Remove previous paragraphs, add new paragraph under h1 and use h3 to display current scores.
    The :hover styles don't work after style attribute
    updates from .js file. Need to figure out why

--- rpsScript.js
+++ rpsScript.js
@@ -30,9 +30,8 @@ function disableButtons() {
     });
 }
 
-let para = document.querySelector('#para');
-let secondPara = document.querySelector('#secondPara');
-let thirdPara = document.querySelector('#thirdPara');
+let playerScoreDisplay = document.querySelector('#playerScore');
+let computerScoreDisplay = document.querySelector('#computerScore');
 
 function resetButtonStyle() {
     computerButtons.forEach(compButton => {
@@ -43,61 +42,60 @@ function resetButtonStyle() {
     });
 }
 
+let para = document.querySelector('#para');
+
 function playRound(playerSelection) {
     resetButtonStyle();
     playerSelection.toString();
     let computerSelection = computerChoice();
-    para.textContent = `You chose ${playerSelection}, the computer chose ${computerSelection}.`;
     
     if (playerSelection === computerSelection) {
-       secondPara.textContent = "You tied!"
+       para.textContent = "You tied!"
     } else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            secondPara.textContent = "You lost, paper beats rock!";
             computerScore++;
+            computerScoreDisplay.textContent = `${computerScore}`;
             computerButtons[1].setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
             buttons.item(0).setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
             } else {
-            secondPara.textContent = "You won, rock beats scissors!";
             playerScore++;
+            playerScoreDisplay.textContent = `${playerScore}`;
             buttons.item(0).setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
             computerButtons[2].setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         }
 
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
-            secondPara.textContent = "You lost, scissors beats paper!";
             computerScore++;
+            computerScoreDisplay.textContent = `${computerScore}`;
             computerButtons[2].setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
             buttons.item(1).setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         } else {
-            secondPara.textContent = "You won, paper beats rock!";
             playerScore++;
+            playerScoreDisplay.textContent = `${playerScore}`;
             buttons.item(1).setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
             computerButtons[0].setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            secondPara.textContent = "You lost, rock beats scissors!";
             computerScore++;
+            computerScoreDisplay.textContent = `${computerScore}`;
             computerButtons[0].setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
             buttons.item(2).setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         } else {
-            secondPara.textContent = "You won, scissors beats paper!";
             playerScore++;
+            playerScoreDisplay.textContent = `${playerScore}`;
             buttons.item(2).setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
             computerButtons[1].setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         }
     }
 
     if (playerScore === 5) {
-        fourthPara.textContent = "You won the game! Refresh the page to play again.";
+        para.textContent = "You won the game! Refresh the page to play again.";
         disableButtons();
     } else if (computerScore === 5) {
-        fourthPara.textContent = "You lost the game! Refresh the page to play again.";
+        para.textContent = "You lost the game! Refresh the page to play again.";
         disableButtons();
     }
-
-    thirdPara.textContent = `Player score: ${playerScore} Computer Score: ${computerScore}`;
 
 }
