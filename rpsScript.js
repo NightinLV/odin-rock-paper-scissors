fd936d438db716239f1343b2732fa7466a9b34e0
NightinLV <laurvrincianu@gmail.com>
2024-03-12 00:12:03 +0100

    Add red/green background, border style to differentiate between win/loss during a round.

--- rpsScript.js
+++ rpsScript.js
@@ -14,6 +14,8 @@ function computerChoice() {
     }
 }
 
+let computerButtons = Array.from(document.querySelectorAll('.computer-buttons'));
+
 let buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
@@ -30,9 +32,19 @@ function disableButtons() {
 
 let para = document.querySelector('#para');
 let secondPara = document.querySelector('#secondPara');
-let thirdPara = document.querySelector('#thirdPara')
+let thirdPara = document.querySelector('#thirdPara');
+
+function resetButtonStyle() {
+    computerButtons.forEach(compButton => {
+        compButton.setAttribute("style", "border: black solid 10px; background-color: #c58c27");
+    });
+    buttons.forEach(button => {
+        button.setAttribute("style", "border: black solid 10px; background-color: #c58c27");
+    });
+}
 
 function playRound(playerSelection) {
+    resetButtonStyle();
     playerSelection.toString();
     let computerSelection = computerChoice();
     para.textContent = `You chose ${playerSelection}, the computer chose ${computerSelection}.`;
@@ -43,26 +55,38 @@ function playRound(playerSelection) {
         if (computerSelection === "paper") {
             secondPara.textContent = "You lost, paper beats rock!";
             computerScore++;
+            computerButtons[1].setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
+            buttons.item(0).setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
             } else {
             secondPara.textContent = "You won, rock beats scissors!";
             playerScore++;
+            buttons.item(0).setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
+            computerButtons[2].setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         }
 
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
             secondPara.textContent = "You lost, scissors beats paper!";
             computerScore++;
+            computerButtons[2].setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
+            buttons.item(1).setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         } else {
             secondPara.textContent = "You won, paper beats rock!";
             playerScore++;
+            buttons.item(1).setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
+            computerButtons[0].setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
             secondPara.textContent = "You lost, rock beats scissors!";
             computerScore++;
+            computerButtons[0].setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
+            buttons.item(2).setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         } else {
-            secondPara.textContent = "You won, paper beats rock!";
+            secondPara.textContent = "You won, scissors beats paper!";
             playerScore++;
+            buttons.item(2).setAttribute("style", "border: #65ff17 solid 10px; background-color: #00ce22");
+            computerButtons[1].setAttribute("style", "border: #7f0018 solid 10px; background-color: #ff0000");
         }
     }
 
